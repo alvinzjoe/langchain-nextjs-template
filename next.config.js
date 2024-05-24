f412500d6a633c878cbdedd4583ecab6bcf93726
Alvin Amri <alvin.amri@42interactive.com>
2024-05-24 12:07:57 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
+const nextConfig = {
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SERPAPI_API_KEY: process.env.SERPAPI_API_KEY,
+    SUPABASE_PRIVATE_KEY: process.env.SUPABASE_PRIVATE_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    LANGCHAIN_TRACING_V2: process.env.LANGCHAIN_TRACING_V2,
+    LANGCHAIN_API_KEY: process.env.LANGCHAIN_API_KEY,
+    LANGCHAIN_PROJECT: process.env.LANGCHAIN_PROJECT
+  }
+};
+module.exports = withBundleAnalyzer(nextConfig)
